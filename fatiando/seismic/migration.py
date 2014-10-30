bdb3bc6f830a2086aa91ed8a3c0c9dfe97d17b40
André <eusoubrasileiro@gmail.com>
2014-10-30 00:09:14 -0200

    Steeping on RTM complete wave equation.

---
+++ fatiando/seismic/migration.py
@@ -0,0 +1,16 @@
+r"""
+Tools for seismic inversion: migration
+
+**Auxiliary functions**
+
+* :func:`~fatiando.seismic.rtm`: apply 2D rtm seismic migration in a zero-offset section
+
+**Theory**
+
+"""
+
+import numpy as np
+
+def rtm():
+
+    return
