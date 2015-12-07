721dbdccb7f76c51ad951e72c104efc225294216
eusoubrasileiro <eusoubrasileiro@gmail.com>
2015-12-06 23:38:05 -0200

    New test with analytic solution.

--- test/test_wavefd.py
+++ test/test_wavefd.py
@@ -4,7 +4,7 @@ from fatiando.seismic.wavefd import (Ricker,
                                      ElasticSH,
                                      ElasticPSV,
                                      Scalar)
-
+from fatiando.seismic import analytic
 import numpy as np
 from numpy.testing import assert_almost_equal
 
@@ -32,22 +32,23 @@ def test_wavefd_elastipsv_run():
                          dip=45, source=Ricker(5, 10, 1./10))
     sim.run(180)
 
-
-def test_wavefd_scalar_run():
-    "run and sum two simulation with different phase wavelets that equals zero"
-    # TODO change to analytic solution comparison
-    shape = (50, 50)
-    velocity = 1500*np.ones(shape)
-    density = 2200*np.ones(shape)
-    sim = Scalar(velocity, (5, 5))
-    sim.add_point_source((shape[0]//2, shape[1]//2), Ricker(5, 10.))
+def test_wavfd_scalar_waveform():
+    "wave form comparison "
+    shape = (60, 50)
+    velocity = 2000*np.ones(shape)
+    sim = Scalar(velocity, (50, 50))
+    fricker = Ricker(1., 2.5, 1.2/2.5)
+    sim.add_point_source((25, 45), fricker)  # far from boundary
     sim.run(100)
-    sim_iphase = Scalar(velocity, (5, 5))
-    sim_iphase.add_point_source((shape[0]//2, shape[1]//2), -1*Ricker(5, 10.))
-    sim_iphase.run(100)
-    diff = sim[-1] + sim_iphase[-1]
-    assert np.all(diff <= 0.01), 'diff: %s' % (str(diff))
-
+    seism = sim[:, 25, 25]
+    t = np.linspace(0.0, sim.dt*sim.simsize, sim.simsize)
+    analy = analytic.wave2d_analytic(20*50, 2000., sim.dt, fricker(t))
+    # normalize everything
+    analy = (analy - analy.min())/(analy.max() - analy.min())
+    seism = (seism - seism.min())/(seism.max() - seism.min())
+    analy -= analy.mean()
+    seism -= seism.mean()
+    assert np.all(abs(analy-seism) <= 0.03)  # 3 percent amp. diff.
 
 def test_wavefd_elasticsh_run():
     "run and sum two simulation with different phase wavelets that equals zero"
